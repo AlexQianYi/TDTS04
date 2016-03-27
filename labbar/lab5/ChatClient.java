fcd0f16b2b6cd0ae5be1fcd719393ce8ddfd41a7
oscpe262 <oscpe262@student.liu.se>
2016-03-27 16:16:00 +0200

    branched and stuff

--- labbar/lab5/ChatClient.java
+++ labbar/lab5/ChatClient.java
@@ -7,158 +7,165 @@ import org.omg.PortableServer.POA;
 import java.util.*;
 
  
-class ChatCallbackImpl extends ChatCallbackPOA
-{
-  private ORB orb;
+class ChatCallbackImpl extends ChatCallbackPOA {
+    private ORB orb;
 
-  public void setORB(ORB orb_val) {
-    orb = orb_val;
-  }
+    public void setORB(ORB orb_val) {
+	orb = orb_val;
+    }
 
-  public void callback(String notification)
-  {
-    System.out.println(notification);
-  }
+    public void callback(String notification){
+	System.out.println(notification);
+    }
 }
 
-public class ChatClient
-{
-  static Chat chatImpl;
+public class ChatClient {
+    static Chat chatImpl;
     
-  public static void main(String args[])
-  {
-    try {
-      // create and initialize the ORB
-      ORB orb = ORB.init(args, null);
+    public static void main(String args[]){
+	try {
+	    // create and initialize the ORB
+	    ORB orb = ORB.init(args, null);
 
-      // create servant (impl) and register it with the ORB
-      ChatCallbackImpl chatCallbackImpl = new ChatCallbackImpl();
-      chatCallbackImpl.setORB(orb);
+	    // create servant (impl) and register it with the ORB
+	    ChatCallbackImpl chatCallbackImpl = new ChatCallbackImpl();
+	    chatCallbackImpl.setORB(orb);
 
-      // get reference to RootPOA and activate the POAManager
-      POA rootpoa = 
-        POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
-      rootpoa.the_POAManager().activate();
+	    // get reference to RootPOA and activate the POAManager
+	    POA rootpoa = 
+		POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+	    rootpoa.the_POAManager().activate();
 	    
-      // get the root naming context 
-      org.omg.CORBA.Object objRef = 
-        orb.resolve_initial_references("NameService");
-      NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+	    // get the root naming context 
+	    org.omg.CORBA.Object objRef = 
+		orb.resolve_initial_references("NameService");
+	    NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
 	    
-      // resolve the object reference in naming
-      String name = "Chat";
-      chatImpl = ChatHelper.narrow(ncRef.resolve_str(name));
+	    // resolve the object reference in naming
+	    String name = "Chat";
+	    chatImpl = ChatHelper.narrow(ncRef.resolve_str(name));
 	    
-      // obtain callback reference for registration w/ server
-      org.omg.CORBA.Object ref = 
-        rootpoa.servant_to_reference(chatCallbackImpl);
-      ChatCallback cref = ChatCallbackHelper.narrow(ref);
+	    // obtain callback reference for registration w/ server
+	    org.omg.CORBA.Object ref = 
+		rootpoa.servant_to_reference(chatCallbackImpl);
+	    ChatCallback cref = ChatCallbackHelper.narrow(ref);
         
-      // Application code goes below
-      String nickname = "";
-      String[] input;
-      Scanner in = new Scanner(System.in);
-      boolean Active = false;
-      boolean Playing = false;
+	    // Application code goes below
+	    String nickname = "";
+	    String[] input;
+	    Scanner in = new Scanner(System.in);
+	    boolean Active = false;
+	    boolean Playing = false;
+
+	    System.out.println("\u001b[32;1m\n" 
+			       + "Welcome to ShitShat!\n"
+			       + "Commands available:\n\u001b[0m"
+			       + "join\n"
+			       + "post\n"
+			       + "list\n"
+			       + "play\n" 
+			       + "put\n"
+			       + "quit\n");
           
-      while(true){
+	    while(true){
               
-        input = in.nextLine().split(" ");
+		input = in.nextLine().split(" ");
 
-        if (input[0].equals("join")){
-          if (input.length < 2){
-            System.out.println("\u001b[31;1m No name given at command line!\u001b[0m");
-          }
-          else if (!Active){
-            nickname = chatImpl.join(cref, input[1]);
-            if (!(nickname.equals("active"))){
-              Active = true;
-            }
-          }
-          else{
-            System.out.println("\u001b[31;1mDon't join twice, " + nickname + "\u001b[0m");
-          }
-        }
+		if (input[0].equals("join")){
+		    if (input.length < 2){
+			System.out.println("\u001b[31;1m No name given at command line!\u001b[0m");
+		    }
+		    else if (!Active){
+			nickname = chatImpl.join(cref, input[1]);
+			if (!(nickname.equals("active"))){
+			    Active = true;
+			}
+		    }
+		    else{
+			System.out.println("\u001b[31;1mDon't join twice, " + nickname + "\u001b[0m");
+		    }
+		}
     
-        if (input[0].equals("post")){
-          if (Active){
-            String msg = "";
-            for(int i= 1; i < input.length; i++) {
-              msg = msg + " " + input[i];
-            }       
-            chatImpl.post(cref, nickname, msg);
-          }
-          else{
-            System.out.println("\u001b[31;1mGo active first!\u001b[0m");
-          }
-        }
+		if (input[0].equals("post")){
+		    if (Active){
+			String msg = "";
+			for(int i= 1; i < input.length; i++) {
+			    msg = msg + " " + input[i];
+			}       
+			chatImpl.post(cref, nickname, msg);
+		    }
+		    else{
+			System.out.println("\u001b[31;1mGo active first!\u001b[0m");
+		    }
+		}
         
-        if (input[0].equals("list")){
-          chatImpl.list(cref, nickname );
-        }
+		if (input[0].equals("list")){
+		    chatImpl.list(cref, nickname );
+		}
         
-        if (input[0].equals("leave")){
-          if (Active){
-            chatImpl.leave(cref, nickname);
-            Active = false;
+		if (input[0].equals("leave")){
+		    if (Active){
+			chatImpl.leave(cref, nickname);
+			Active = false;
 
-            if (Playing){
-              chatImpl.leaveGame(nickname); 
-              Playing = false;
-            }
-          }
-          else{
-            System.out.println("\u001b[31;1mJoin before leaving!\u001b[0m");
-          }
-        }     
+			if (Playing){
+			    chatImpl.leaveGame(nickname); 
+			    Playing = false;
+			}
+		    }
+		    else{
+			System.out.println("\u001b[31;1mJoin before leaving!\u001b[0m");
+		    }
+		}     
 
-        if(input[0].equals("play")){
-          if (Active){
-            if (input.length > 1){
-              String color = input[1].substring(0,1);
-              chatImpl.play(cref, nickname, color);
-              Playing = true;
-            }
-          }
-          else{
-            System.out.println("\u001b[31;1mJoin first omg\u001b[0m");
-          }
-        }
-        
-        if (input[0].equals("put")){
-          if (Playing){
-            if (input.length > 1){
-              String pos = input[1];
-              if (pos.matches("\\d{2}")){
-                chatImpl.put(cref, nickname, pos);
-              }
-              else{
-                System.out.println("\u001b[31;1mOnly 2-digit numeric chars allowed\u001b[0m");
-              }
-            }
-            else{
-              System.out.println("Enter a 2-digit coordinate to put");
-            }
-          }
-          else{
-            System.out.println("\u001b[31;1mJoin a game first\u001b[0m");
-          }
-        }
+		if(input[0].equals("play")){
+		    if (Active){
+			if (input.length > 1){
+			    String color = input[1].substring(0,1);
+			    chatImpl.play(cref, nickname, color);
+			    Playing = true;
+			}
+		    }
+		    else{
+			System.out.println("\u001b[31;1mJoin first omg\u001b[0m");
+		    }
+		}
+		//Note-to-self: int (char(a)) == 97
+		if (input[0].equals("put")){
+		    if (Playing){
+			if (input.length > 1){
+			    String pos = input[1];
+			    if (pos.matches("([a-h]|[A-H])+([1-8])")){
+				chatImpl.put(cref, nickname, pos);
+			    }
+			    else{
+				System.out.println("\u001b[31;1mSyntax: \"put [a-h][1-8]\", i.e. \"put a3\"\u001b[0m");
+			    }
+			}
+			else{
+			    System.out.println("Enter a 2-digit coordinate to put");
+			}
+		    }
+		    else{
+			System.out.println("\u001b[31;1mJoin a game first\u001b[0m");
+		    }
+		}
 
-        if (input[0].equals("quit")){
-          if  (!Active){
-            System.out.println("Bye " + nickname + "!");
-            System.exit(0);
-          }
-          else{
-            System.out.println(nickname + "\u001b[31;1mActive, leave first!\u001b[0m");
-          }
-        } 
-      }              
+		if (input[0].equals("quit")){
+		    if (Active){
+			chatImpl.leave(cref, nickname);
+			Active = false;
+			System.out.println("\u001b[31;1mStill active, leaving ...\u001b[0m");
+		    }
+
+		    System.out.println("\u001b[36mBye " + nickname + "!\u001b[0m");
+		    System.exit(0);
+		} 
+	    }              
+	}
+	catch(Exception e){
+	    System.out.println("\u001b[31;1mERROR : " + e + "\u001b[0m");
+	    e.printStackTrace(System.out);
+	}
     }
-    catch(Exception e){
-      System.out.println("\u001b[31;1mERROR : " + e + "\u001b[0m");
-      e.printStackTrace(System.out);
-     }
-  }
 }
--- labbar/lab5/ChatServer.java
+++ labbar/lab5/ChatServer.java
@@ -10,8 +10,7 @@ import java.util.*;
 For each method Callbacking, add try-catch<client.remove> 
  */
 
-class ChatImpl extends ChatPOA
-{
+class ChatImpl extends ChatPOA {
   private ORB orb;
   Map<String, ChatCallback> clients = new HashMap<String, ChatCallback>();
       Game game = new Game();
@@ -20,111 +19,127 @@ class ChatImpl extends ChatPOA
     orb = orb_val;
   }
   
-  public String join(ChatCallback objref, String nickname)
-  {    
-    if(clients.containsKey(nickname)){
-      objref.callback("\u001b[31;1m" + nickname + " is already an active chatter\u001b[0m");
-      return "active";
-    }
-    for (ChatCallback callback : clients.values())
-    {
-      callback.callback(nickname + " has joined!"); // goes out to everyone
-    }
+    public String join(ChatCallback objref, String nickname){    
+	if(clients.containsKey(nickname)){
+	    objref.callback("\u001b[31;1m" + nickname + " is already an active chatter\u001b[0m");
+	    return "active";
+	}
+	for (ChatCallback callback : clients.values()) {
+	    try {
+		callback.callback("\u001b[33m" + nickname + " has joined!\u001b[0m"); // goes out to everyone
+	    }
+	    catch(Exception e){
+		System.out.println("\u001b[31;1mLost connection to peer! \u001b[0m");
+	    }
+	}
     
-    objref.callback("Welcome " + nickname);
-    clients.put(nickname, objref);
-    return nickname;
+	objref.callback("\u001b[36mWelcome " + nickname + "!\u001b[0m");
+	clients.put(nickname, objref);
+	return nickname;
     
-  }
+    }
   
-  public void post(ChatCallback objref, String nickname, String msg)
-  {
-    for (ChatCallback callback : clients.values())
-    {
-      callback.callback(nickname + " said: " + msg);
-    }   
-  }
+    public void post(ChatCallback objref, String nickname, String msg){
+	for (ChatCallback callback : clients.values()) {
+	    try {
+		callback.callback("\u001b[34;1m" + nickname + ":\u001b[0m" + msg);
+	    }
+	    catch(Exception e){
+		System.out.println("\u001b[31;1mWe've lost a peer! \u001b[0m");
+	    }
+	    
+	}   
+    }
   
-  public void list(ChatCallback objref, String nickname)
-  {
-    objref.callback("List of registered users: ");
+    public void list(ChatCallback objref, String nickname){
+	objref.callback("\u001b[36mList of registered users: \u001b[0m");
        
-    for(String joinedNicks : clients.keySet()){
-      objref.callback(joinedNicks);
+	for(String joinedNicks : clients.keySet()){
+	    objref.callback(joinedNicks);
+	}
     }
-  }
 
-  public void leave(ChatCallback objref, String nickname) 
-  {
-    clients.remove(nickname); // remove post in hash
-    for (ChatCallback callback : clients.values()) 
-    {
-      callback.callback(nickname + " has left."); // broadcast message
+    public void leave(ChatCallback objref, String nickname){
+	clients.remove(nickname); // remove post in hash
+	for (ChatCallback callback : clients.values()) {
+	    try {
+		callback.callback("\u001b[33m" + nickname + " has left.\u001b[0m"); // broadcast message
+	    }
+	    catch(Exception e){
+		System.out.println("\u001b[31;1mWe've lost a peer! \u001b[0m");
+	    }
+	}
+	
+	objref.callback("Cheers " + nickname);
     }
-    
-    objref.callback("Cheers " + nickname);
-  }
 
     
-  public void play(ChatCallback objref, String nickname, String color)
-  {
-    game.join(objref, nickname, color);
-  }
-  public void put(ChatCallback objref, String nickname, String pos)
-  {
-    game.put(objref, nickname, pos); 
-  }
-  public void leaveGame(String nickname)
-  {
-    game.leave(nickname);
-  }
-    
+    public void play(ChatCallback objref, String nickname, String color){
+	game.join(objref, nickname, color);
+    }
+
+    public void put(ChatCallback objref, String nickname, String pos){
+	game.put(objref, nickname, pos); 
+    }
+
+    public void leaveGame(String nickname){
+	game.leave(nickname);
+    }
+
+    /*
+    public voic pass(String nickname){
+	game.pass(nickname);
+    }
+
+    public void reset() {
+        game.reset();
+    }
+
+    */
 }
 
-public class ChatServer 
-{
-  public static void main(String args[]) 
-  {
-    try { 
-      // create and initialize the ORB
-      ORB orb = ORB.init(args, null); 
+public class ChatServer {
+    public static void main(String args[]) {
+	try { 
+	    // create and initialize the ORB
+	    ORB orb = ORB.init(args, null); 
 
-      // create servant (impl) and register it with the ORB
-      ChatImpl chatImpl = new ChatImpl();
-      chatImpl.setORB(orb); 
+	    // create servant (impl) and register it with the ORB
+	    ChatImpl chatImpl = new ChatImpl();
+	    chatImpl.setORB(orb); 
 
-      // get reference to rootpoa & activate the POAManager
-      POA rootpoa = 
-        POAHelper.narrow(orb.resolve_initial_references("RootPOA"));  
-      rootpoa.the_POAManager().activate(); 
+	    // get reference to rootpoa & activate the POAManager
+	    POA rootpoa = 
+		POAHelper.narrow(orb.resolve_initial_references("RootPOA"));  
+	    rootpoa.the_POAManager().activate(); 
 
-      // get the root naming context
-      org.omg.CORBA.Object objRef = 
-        orb.resolve_initial_references("NameService");
-      NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+	    // get the root naming context
+	    org.omg.CORBA.Object objRef = 
+		orb.resolve_initial_references("NameService");
+	    NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
 
-      // obtain object reference from the servant (impl)
-      org.omg.CORBA.Object ref = 
-        rootpoa.servant_to_reference(chatImpl);
-      Chat cref = ChatHelper.narrow(ref);
+	    // obtain object reference from the servant (impl)
+	    org.omg.CORBA.Object ref = 
+		rootpoa.servant_to_reference(chatImpl);
+	    Chat cref = ChatHelper.narrow(ref);
 
-      // bind the object reference in naming
-      String name = "Chat";
-      NameComponent path[] = ncRef.to_name(name);
-      ncRef.rebind(path, cref);
+	    // bind the object reference in naming
+	    String name = "Chat";
+	    NameComponent path[] = ncRef.to_name(name);
+	    ncRef.rebind(path, cref);
 
-      System.out.println("ChatServer ready and waiting ...");
+	    System.out.println("\u001b[32;1m\nChatServer ready and waiting ...\u001b[0m");
 	    
-      // wait for invocations from clients
-      orb.run();
-    }
+	    // wait for invocations from clients
+	    orb.run();
+	}
 	    
-    catch(Exception e) {
-      System.err.println("\u001b[31;1mERROR : " + e + "\u001b[30m");
-      e.printStackTrace(System.out);
-    }
+	catch(Exception e) {
+	    System.err.println("\u001b[31;1mERROR : " + e + "\u001b[0m");
+	    e.printStackTrace(System.out);
+	}
 
-    System.out.println("ChatServer Exiting ...");
-  }
+	System.out.println("ChatServer Exiting ...");
+    }
 
 }
